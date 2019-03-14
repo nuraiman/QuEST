12ba95f1947d0f96f83342fe1d4fcd35dfd9abfc
Giulia Mazzola <giuli.mazzola@gmail.com>
2019-03-14 14:18:32 +0100

    changes in shor.cpp

--- examples_giulia/shor.cpp
+++ examples_giulia/shor.cpp
@@ -23,35 +23,52 @@ int sample_uniformly(int low = 0, int high = 1) {
     return distribution(mt);
 }
 
-// Assuming that N is odd and not a power of a prime!
-int ShorFactoring(int N){
-    //load quest
-    QuESTEnv env = createQuESTEnv();
+// The Algorithm assumes that N is odd and not a power of a prime
+int ShorFactoring(int N) {
+    // check if N is odd
+    if (N % 2 == 0) {
+        return 2;
+    }
 
+    // check if N is a power of a prime
+    int p;
+    for (int k = 2; k <= std::log2(N); ++k) {
+        p = (int) std::pow(N,1.0/k);
+        if (N %  p == 0){
+            return p;
+        }
+    }
+
+    // shor's algorithm
     int a = sample_uniformly(2, N-1);
     int b = gcd(a, N);
-//    std::cout << b << std::endl ? b > 1 : ;
     if (b > 1){
         return b;
     }
+
+    //load quest
+    QuESTEnv env = createQuESTEnv();
     int r = PeriodFinding(env, a, N);
     if (r % 2 == 1){
+        destroyQuESTEnv(env);
         return -1;
     }
     r /= 2;
     b = int((long long) pow(a,r));;
     int s = gcd(b, N);
     if (s == 1){
+        destroyQuESTEnv(env);
         return -1;
     }
+    destroyQuESTEnv(env);
     return s;
 }
 
 int main(int narg, char *varg[]) {
-    int N = 55;
+    int N = 21;
     int factor = ShorFactoring(N);
-    if (factor>0) {
-        std::cout << "Factors found: " << factor << ",  " << N/factor << std::endl;
+    if (factor > 0) {
+        std::cout << "Factors found: " << factor << ",  " << N / factor << std::endl;
     }
     else {
         std::cout << "Algorithm failed" << std::endl;
