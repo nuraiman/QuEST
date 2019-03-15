3504f090b8f7c193bee7ec4fec9256575ea888a7
Marko Kabic <marko.kabic@cscs.ch>
2019-03-15 12:50:58 +0100

    From int to long long.

--- examples_giulia/shor.cpp
+++ examples_giulia/shor.cpp
@@ -1,10 +1,24 @@
 #include "shor.hpp"
+#include "cxxopts/cxxopts.hpp"
 
 int main(int narg, char *varg[]) {
     //load quest
     QuESTEnv env = createQuESTEnv();
+    cxxopts::Options desc(argv[0], "Allowed Options");
+    desc.add_options()
+        ("help", "produce help message")
+        ("N,number", "Number to factorize.", cxxopts::value<long long>()->default_value("21"))
+        ("r,repetitions", "Number of repetitions", cxxopts::value<int>()->default_value("1"));
 
-    long long N = 2201;
+    auto vm = desc.parse(argc, argv);
+
+    if (vm.count("help")) {
+        std::cout << desc.help() << std::endl;
+        return 1;
+    }
+
+    const long long N = vm["N"].as<long long>();
+    const int r = vm["r"].as<int>();
 
     syncQuESTEnv(env);
     auto start = std::chrono::steady_clock::now();
