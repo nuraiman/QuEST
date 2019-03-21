4aff425a7337497b938dd0e0f3569cef250af8c8
Giulia Mazzola <giuli.mazzola@gmail.com>
2019-03-21 16:41:31 +0100

    added repetitions in grover.cpp for benchmarking

--- examples_giulia/grover.cpp
+++ examples_giulia/grover.cpp
@@ -20,50 +20,77 @@ int main(int narg, char *varg[]) {
     const qInt N = vm["N"].as<qInt>();
     const int nrep = vm["r"].as<int>();
 
-    syncQuESTEnv(env);
-    auto start = std::chrono::steady_clock::now();
-    // find smallest natural number n such that N <= 2^n
-    int n = int(ceil(log2(N)));
+    unsigned long int seed =1;
+    seedQuEST(&seed,1);
 
-    std::cout << "n = " << n << std::endl;
-    // choose marked element at random
-    qInt x0 = sample_uniformly(0,N-1);
-    std::cout << "Test x0 = " << x0 << std::endl;
+    // times vector for multiple repetitions of the code
+    std::vector<long long> times;
+    times.reserve(nrep);
 
-
-    // Create quantumregister and initialize in the |++...+> state
-    Qureg qureg = createQureg(n, env);
-    initPlusState(qureg);
-
-    // calculate optimal number T of iterations
-    int T = int(round(pi*0.25*sqrt(1<<n)-0.5));
-    std::cout << "T = " << T << std::endl;
-
-    // Grover algorithm
-    for (int i = 0; i < T ; ++i) {
-        PhaseOracle(qureg, x0);
-        GroverStep(qureg, n);
-    }
-
-    // measure all the qubits and save their values in the variable 'result'
-    qreal success = getProbAmp(qureg, x0);
+    std::vector<qInt> factors;
 
     qInt result = 0;
-    int a;
-    for (int j = 0; j < n ; ++j) {
-        a = measure(qureg,j);
-        if (a==1){
-            result = result | (1 << j);
+    qreal success = 0;
+    for (int i = 0; i < nrep; ++i) {
+        syncQuESTEnv(env);
+        auto start = std::chrono::steady_clock::now();
+        // find smallest natural number n such that N <= 2^n
+        int n = int(ceil(log2(N)));
+
+        std::cout << "n = " << n << std::endl;
+        // choose marked element at random
+        qInt x0 = sample_uniformly(0,N-1,seed);
+        std::cout << "Test x0 = " << x0 << std::endl;
+
+
+        // Create quantumregister and initialize in the |++...+> state
+        Qureg qureg = createQureg(n, env);
+        initPlusState(qureg);
+
+        // calculate optimal number T of iterations
+        int T = int(round(pi*0.25*sqrt(1<<n)-0.5));
+        std::cout << "T = " << T << std::endl;
+
+        // Grover algorithm
+        for (int j = 0; j < T ; ++j) {
+        PhaseOracle(qureg, x0);
+        GroverStep(qureg, n);
         }
+
+        // measure all the qubits and save their values in the variable 'result'
+        success = getProbAmp(qureg, x0);
+
+        //qInt result = 0;
+        int a;
+        for (int j = 0; j < n ; ++j) {
+            a = measure(qureg,j);
+            if (a==1){
+                result = result | (1 << j);
+            }
+        }
+
+        syncQuESTEnv(env);
+        auto end = std::chrono::steady_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+        times.push_back(duration);
+        destroyQureg(qureg, env);
     }
 
-    syncQuESTEnv(env);
-    auto end = std::chrono::steady_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
     std::cout << "The algorithm found x0 = " << result << " with success probability " << success << std::endl;
-    std::cout << "Duration [ns] = " << duration << std::endl;
 
-    destroyQureg(qureg, env);
+    // output the duration vector over repetitions
+    std::cout << "Duration [ns] = ";
+    for (int i = 0; i < nrep; ++i) {
+        std::cout << times[i];
+        if (i < nrep - 1) {
+            std::cout << ", ";
+        }
+    }
+    std::cout << std::endl;
+
+    auto statistics = computeStatistics(times);
+    std::cout << "Avg time [ns] = " << statistics.first << ", Std deviation = " << statistics.second << std::endl;
+
     destroyQuESTEnv(env);
     return 0;
 }
